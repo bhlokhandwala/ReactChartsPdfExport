a5a2a9af6381cd912f0af4da05ce46bea373a4eb
burhanMedlabz <burhanuddin.lokhandwala@medlabz.com>
2021-06-12 07:44:43 +0530

    PDF download with chart js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Bar } from "react-chartjs-2";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  cData = {
+    labels: ["L 1", "L 2", "L 3", "L 4", "L 5"],
+    datasets: [
+      {
+        label: "Label",
+        data: [100, 150, 123, 170, 162],
+        backgroundColor: ["red", "green", "yellow", "blue", "orange", "red"]
+      }
+    ]
+  };
+
+  div2PDF = e => {
+    const but = e.target;
+    but.style.display = "none";
+    let input = window.document.getElementsByClassName("div2PDF")[0];
+
+    html2canvas(input).then(canvas => {
+      const img = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("l", "pt");
+      pdf.addImage(
+        img,
+        "png",
+        input.offsetLeft,
+        input.offsetTop,
+        input.clientWidth,
+        input.clientHeight
+      );
+      pdf.save("chart.pdf");
+      but.style.display = "block";
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="div2PDF" style={{height:'400px', width:'400px'}}>
+          <Bar
+            data={this.cData}
+            options={{
+              title: {
+                display: true,
+                text: "Chart to PDF Demo",
+                fontSize: 32
+              },
+              legend: {
+                display: true,
+                position: "right"
+              }
+            }}
+            height={200}
+          />
+          <h1>burhan</h1>
+        </div>
+        <div>
+          <button onClick={e => this.div2PDF(e)}>Export 2 PDF</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
